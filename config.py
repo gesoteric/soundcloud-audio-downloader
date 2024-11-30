818dfdff2e29fa9db7acd5c3c04668fb1ec8621f
gesoteric <gesoteric1337@gmail.com>
2024-11-30 15:28:05 +1100

    config template file

---
+++ config.py
@@ -0,0 +1,8 @@
+# Configuration file for user and client details
+
+user_id = 123456  # Replace with the actual user ID
+client_id = "YOUR_CLIENT_ID"  # Replace with your SoundCloud client ID
+limit = 101
+offset = 0
+MAX_PARALLEL_TASKS = 5  # WIP 
+
